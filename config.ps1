1eed6c3ab8577e40a68db84134515a5f3296243a
Tengiz Sharafiev <btolfa@gmail.com>
2018-04-06 21:19:23 +0700

    Bump openssl version up to 1.1.0h

--- config.ps1
+++ config.ps1
@@ -1,6 +1,6 @@
 #$ErrorActionPreference = "Stop"
 
-$global:OpenSSL_version = "1_1_0g"
+$global:OpenSSL_version = "1_1_0h"
 $global:OpenSSL_file = "OpenSSL_$OpenSSL_version.zip"
 $global:OpenSSL_sourcedir = "openssl-OpenSSL_$OpenSSL_version"
 $global:OpenSSL_url = "https://github.com/openssl/openssl/archive/$OpenSSL_file"
